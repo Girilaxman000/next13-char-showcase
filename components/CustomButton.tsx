d1bf7dc1787dc114df8878a45c12cca20ada2c83
Girilaxman000 <kesharigiri000@gmail.com>
2023-06-13 14:55:38 +0545

    feat: created hero and footer section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { ICustomButtonProps } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: ICustomButtonProps) => {
+  return (
+    <button
+      className={`custom-btn ${containerStyles}`}
+      disabled={false}
+      type={btnType || "button"}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+};
+
+export default CustomButton;
---
+++ components/index.ts
@@ -0,0 +1,7 @@
+//using this file as a single file that's going to export all of our components
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+export { Hero, CustomButton, Footer, Navbar };
